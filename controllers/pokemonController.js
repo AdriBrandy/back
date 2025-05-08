60dc6c54415570ae6f07c3e2bb920839b29e1cbb
Adri <adriana_brandan22@outlook.com>
2025-05-08 03:04:29 -0300

    *

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,7 +1,8 @@
-
+require("dotenv").config();
 const NodeCache = require("node-cache");
 
-// Caché con TTL de 1 hora (3600 segundos)
+const POKEAPI_URL = process.env.POKEAPI_URL;
+
 const pokemonCache = new NodeCache({ stdTTL: 3600 });
 
 const getAllPokemons = async (req, res) => {
@@ -11,9 +12,9 @@ const getAllPokemons = async (req, res) => {
       return res.json(cachedList);
     }
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const response = await fetch(`${POKEAPI_URL}?limit=151`);
     const data = await response.json();
-    pokemonCache.set("pokemonList", data.results); // Guardamos en caché
+    pokemonCache.set("pokemonList", data.results);
     res.json(data.results);
   } catch (error) {
     res.status(500).json({ message: "Error al obtener Pokémon", error });
@@ -23,14 +24,13 @@ const getAllPokemons = async (req, res) => {
 const getPokemonById = async (req, res) => {
   const { id } = req.params;
 
-  // Revisamos si ya está en caché
   const cachedPokemon = pokemonCache.get(`pokemon_${id}`);
   if (cachedPokemon) {
     return res.json(cachedPokemon);
   }
 
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const response = await fetch(`${POKEAPI_URL}/${id}`);
     
     if (!response.ok) {
       return res.status(response.status).json({ message: "Error al obtener Pokémon de la API externa" });
